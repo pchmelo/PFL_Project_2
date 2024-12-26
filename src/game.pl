662bbc3682f1ae8232ed8100777a6b86bc58108f
Vasco Melo <vascorochamelo@gmail.com>
2024-12-26 11:21:18 +0000

    Second Commit

---
+++ src/game.pl
@@ -0,0 +1,13 @@
+% initial_state(+GameConfig, -GameState)
+
+% display_game(+GameState)
+
+% move(+GameState, +Move, -NewGameState)
+
+% valid_moves(+GameState,  -ListOfMoves)
+
+% game_over(+GameState,  -Winner)
+
+% value(+GameState, +Player, -Value)
+
+% choose_move(+GameState,  +Level,  -Move)

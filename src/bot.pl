a9af6fe84bd54ac6940e1bedb286384c74d40fc2
Vasco Melo <vascorochamelo@gmail.com>
2025-01-03 21:46:07 +0000

    bot.pl added

--- src/bot.pl
+++ src/bot.pl
@@ -1,13 +1,26 @@
+/*
+    parameters: GameState
+    return: coordinates
+    this function returns a random valid move from the list of valid moves, for the easy bot
+*/
 easy_bot_move(GameState, X, Y) :-
     valid_moves(GameState, ListOfMoves),
     random_member((X, Y), ListOfMoves).
 
-% Define the medium bot move
+/*
+    parameters: GameState, Player, Char
+    return: coordinates
+    this function returns the best move from the list of valid moves, for the medium bot. The best move is obtaind by evaluating the game state after each move possible and selecting the move that gives the best value for the Player (Medium Bot)
+*/
 medium_bot_move(GameState, Player, Char, X, Y) :-
     valid_moves(GameState, ListOfMoves),
     find_best_move(GameState, Player, Char, ListOfMoves, (X, Y)).
 
-% Find the best move from the list of valid moves
+/*
+    parameters: GameState, Player, Char, ListOfMoves
+    return: coordinates
+    this function returns the best move from the list of valid moves. First gets the Gamestate resulted from each move possible, then evaluates the game state and save it in a list. Finally, it selects a random move the received list of best moves 
+*/
 find_best_move(GameState, Player, Char, [(X, Y)|Tail], (Final_X, Final_Y)) :-
     move(GameState, ((X, Y), Char), NewGameState),
     change_score(NewGameState, ((X, Y), Char), NewGameStateScored),
@@ -15,7 +28,11 @@ find_best_move(GameState, Player, Char, [(X, Y)|Tail], (Final_X, Final_Y)) :-
     find_best_move(GameState, Player, Char, Tail, [(X, Y)], Value, BestMoves),
     random_member((Final_X, Final_Y), BestMoves).
 
-% Base case: no more moves to check
+/*
+    parameters: GameState, Player, Char, ListOfMoves, CurrentBestMoves, CurrentBestValue, BestMoves
+    return: coordinates
+    this function finds the best move from the list of valid moves. It evaluates the score of the game state, if the score is better than the current best score, it updates the best score and the best move. If the score is equal to the current best score, it adds the move to the list of best moves. If the score is worse than the current best score, it keeps the current best score and the current best moves. It uses recursion to check the next move
+*/
 find_best_move(_, _, _, [], BestMoves, _, BestMoves).
 
 % Recursive case: check the next move
@@ -25,7 +42,11 @@ find_best_move(GameState, Player, Char, [(X, Y)|Tail], CurrentBestMoves, Current
     value(NewGameStateScored, Player, Value),
     compare_moves(Value, CurrentBestValue, GameState, Player, Char, Tail, (X, Y), CurrentBestMoves, BestMoves).
 
-% Compare the current move with the best move
+/*
+    parameters: Value, CurrentBestValue, GameState, Player, Char, Tail, (X, Y), BestMoves
+    return: CurentBestMoves
+    this function checks if the value of the game state is better than the current best value. If it is, it updates the best value and the best moves. If the value is equal to the current best value, it adds the move to the list of best moves. If the value is worse than the current best value, it keeps the current best value and the current best moves
+*/
 compare_moves(Value, CurrentBestValue, GameState, Player, Char, Tail, (X, Y), _, BestMoves) :-
     Value > CurrentBestValue,
     find_best_move(GameState, Player, Char, Tail, [(X, Y)], Value, BestMoves).
@@ -37,7 +58,11 @@ compare_moves(Value, CurrentBestValue, GameState, Player, Char, Tail, _, Current
     Value < CurrentBestValue,
     find_best_move(GameState, Player, Char, Tail, CurrentBestMoves, CurrentBestValue, BestMoves).
 
-% Evaluate the game state based on the player 1
+/*
+    parameters: GameState, Player (1 - Player 1 and 2 - Player 2)
+    return: Value
+    this function returns the value of the game state based on the score of the player 1 or player 2 
+*/
 value(game_state(_, Player1Score, Player2Score, _, _, _, _, _, _, _, _, _), 1, Value) :-
     Value is Player2Score - Player1Score.
 
